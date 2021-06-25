047811af7fc26d576999da51d210233dd237dd93
Juan Sebastian Moreno Hernandez <juanse.jm18@gmail.com>
2021-06-24 21:29:15 -0500

    REFACTOR: User admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,7 +1,6 @@
 """Accounts admin"""
 
 # Django
-from django import forms
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -10,49 +9,24 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from apps.accounts.models import User
 
 
-class UserCreationForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = "__all__"
-        widgets = {
-            "groups": forms.HiddenInput(),
-            "user_permissions": forms.HiddenInput(),
-        }
-
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_unusable_password()
-        if commit:
-            user.save()
-        return user
-
-
 class UserAdmin(BaseUserAdmin):
     """Custom admin model from django user admin model."""
 
-    add_form = UserCreationForm
-
     fieldsets = (
-        (_("Personal information"), {"fields": ("first_name", "last_name")}),
-        (_("Account information"), {"fields": ("email", "is_active", "is_staff", "is_superuser")}),
+        (
+            _("Personal information"),
+            {"fields": ("first_name", "last_name", "email", "phone", "address")},
+        ),
+        (
+            _("Account information"),
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
-    readonly_fields = ("last_login", "date_joined")
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "first_name",
-                    "last_name",
-                    "email",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
+        (None, {"fields": ("first_name", "last_name", "email", "password1", "password2")}),
     )
+    readonly_fields = ("last_login", "date_joined")
     list_display = ("email", "first_name", "last_name", "is_staff", "last_login")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
