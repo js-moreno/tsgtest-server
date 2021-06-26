53208b5d7198356c74b8a5e8d4598fed0461049b
Juan Sebastian Moreno Hernandez <juanse.jm18@gmail.com>
2021-06-26 11:41:33 -0500

    FEAT: User creation no authorization required

--- apps/accounts/views/users.py
+++ apps/accounts/views/users.py
@@ -3,6 +3,10 @@
 
 # Django Rest Framework
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+# Drf-yasg
+from drf_yasg.utils import swagger_auto_schema
 
 # Models
 from apps.accounts.models import User
@@ -20,3 +24,14 @@ class UserViewSet(viewsets.ModelViewSet):
         if not self.request.user.is_superuser:
             return User.objects.filter(pk=self.request.user.id)
         return User.objects.all()
+
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    @swagger_auto_schema(security=[])
+    def create(self, request, *args, **kwargs):
+        return super(UserViewSet, self).create()
